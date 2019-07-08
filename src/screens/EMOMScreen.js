29e107dc9125fee643226a7efc793a960337b3fb
Erick Bernardo <erickvictor@gmail.com>
2019-07-08 12:37:17 -0400

    Add assets fonts and Navigate

---
+++ src/screens/EMOMScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const EMOMScreen = props => {
+  return(
+    <View>
+      <Text>EMOM Screen</Text>
+    </View>
+  )
+}
+export default EMOMScreen
