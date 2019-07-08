868812f51eccb39e8d31074ab770ce7ca2da92d2
Erick Bernardo <erickvictor@gmail.com>
2019-07-08 14:35:15 -0400

    Correct EMOM import console

--- src/screens/EMOMScreen.js
+++ src/screens/EMOMScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import console = require('console');
 
 class Select extends Component {
   state = {
